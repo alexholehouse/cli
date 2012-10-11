88010ec448154fe0a1574f839b8b66894cfb8f62
rednaxela <alex.holehouse@gmail.com>
2012-10-11 18:29:52 -0500

    Added even more tests and some additional functionality, notably the 'yes, no or abort' function, as well as improved formatting for error and warning messages

--- cli/cli.py
+++ cli/cli.py
@@ -5,10 +5,10 @@ import sys
 from datetime import datetime
 
 #--------------------------------------------------------
+# Exception class for cli
+#
 class CommandlineInterfaceError(BaseException):
     pass
-
-
 
 #--------------------------------------------------------
 # Print a row of $character, the width of the terminal,
@@ -24,27 +24,40 @@ def print_terminal_line(character, offset=1):
     else:
         print character * int(columns)
 
-        
+
+# check
+#--------------------------------------------------------
+# Print a message in the middle of the terminal with terminal
+# border set to $border
+#
+#        
 def print_in_middle(message, border=" "):
     rows, columns = os.popen('stty size', 'r').read().split()
     length_of_message = len(message)
     offset  = 1 + length_of_message%2
     print border + " " * int((float(columns)/2-(float(length_of_message)/2))) + message + " " * int(float(columns)/2-(float(length_of_message)/2)-offset) + border
 
-def big_message(message, seperator="#"):
+
+#--------------------------------------------------------
+# Print a header message which looks something like this 
+#
+##################################################
+#              message contents                  #
+##################################################
+#
+# 
+def print_big_message(message, seperator="#"):
     print_terminal_line(seperator)
     print_in_middle(message, seperator)
     print_terminal_line(seperator)
 
-    
 
-
-
+#
+# yes_or_no()
 #--------------------------------------------------------
 # Asks if the user wants to continue or not. $default can 
 # be "N" or "Y" to specifiy if the default behavour is
 # true or false
-
 #
 def yes_or_no(message, default="N"):
     
@@ -74,23 +87,87 @@ def yes_or_no(message, default="N"):
     else:
         error_message = "ERROR in cli.yes_or_no() - invalid default specified: '" + default + "'"
         raise CommandlineInterfaceError(error_message);
+#
+# yes_or_no_or_abort()
+# --------------------------------------------------------
+# Asks if the user wants to continue or not. If they fail
+# to select either option, (y/Y or n/N) then the script
+# aborts.
+#
+def yes_or_no_or_abort(message):
+    
+    try:
+        input_command = raw_input(message + " [y/n/A]: ")
+    except KeyboardInterrupt:
+        input_command = "n"
+        
+    if(input_command == "y" or input_command == "Y"):
+        return True
 
+    if(input_command == "n" or input_command == "N"):
+        return False
+                
+    else:
+        AutoAbort()
+ 
+#
+# clear_screen()
+# --------------------------------------------------------
+# Clear the screen
+#
 def clear_screen():
     os.system('clear')
 
-def error_and_quit(message):
-    print "[FATAL ERROR] " + message
-    print "Exiting ungracefully...\n"
+
+#
+# error_and_quit()
+# --------------------------------------------------------
+# Display an error and quit with status (1)
+#
+def error_and_quit(message=" "):
+    print str(datetime.now()) + "| [FATAL ERROR] \n" + message
+    print "\nExiting ungracefully...\n"
     exit(1)
 
-def error_and_continue(message):
-    print "[ERROR] " + message
-    
-def warning_and_continue(message):
-    print "[WARNING] " + message
 
+#
+# clear_and_continue()
+# --------------------------------------------------------
+# Display an error message but don't stop. This may not 
+# be a good thing to do (errors should quit). Maybe a 
+# warning would be better.
+#
+def error_and_continue(message):
+    print str(datetime.now()) + "| [ERROR] \n" + message
+    print "\n"
+
+#
+# warning_and_continue()
+# --------------------------------------------------------
+# Display a warning message and quit
+#   
+def warning_and_continue(message):
+    print str(datetime.now()) + "| [WARNING] \n" + message
+    print "\n"
+
+
+#
+# clear_screen()
+# --------------------------------------------------------
+# Clear the screen
+#
 def status_update(message):
     print str(datetime.now()) + "| " + message
+
+
+#
+# clear_screen()
+# --------------------------------------------------------
+# Clear the screen
+#
+def autoAbort():
+    print str(datetime.now()) + "| Aborting..."
+    exit(0)
     
 
 
--- tests/clitests.py
+++ tests/clitests.py
@@ -7,30 +7,67 @@ class TestCLIFunctions(unittest.TestCase):
     def setUp(self):
         self.displayString = "This is the test string"
         pass
-        
-    def test_Example(self):
-        self.assertEquals(1,1)
+
+    def seperator(self):
+        ## this is so meta, but use the print_terminal_line to help deliniate tests!
+        print("\n")
+        cli.print_terminal_line("_")
 
     def test_print_terminal_line(self):
-        print "\nBelow should be a line of '!' which extend for the length of the window. Try rerunning at different sizes"
+        print "\nBelow should be a line of '!' which extend for the length of the window. Try rerunning at different sizes\n\n"
         cli.print_terminal_line("!")
+        self.seperator()
 
     def test_print_terminal_line_with_ofset(self):
-        print "\nBelow should be a line of '!' which extend for the length of the window. Try rerunning at different sizes"
+        print "\nBelow should be a line of '!' which extend for the length of the window. Try rerunning at different sizes\n\n"
         cli.print_terminal_line("!", 0.5)
+        self.seperator()
 
     def test_print_in_middle(self):
-        print "\nBelow should be the message [" + self.displayString  + "] in the middle of the terminal window. Try rerunning at different sizes\n"
+        print "\nBelow should be the message [" + self.displayString  + "] in the middle of the terminal window. Try rerunning at different sizes\n\n"
         cli.print_in_middle(self.displayString)
-        print "\n"
+        self.seperator()
     
     def test_print_in_middle_with_border(self):
-        print "\nBelow should be the message [" + self.displayString  + "]  in the middle of the terminal window. Try rerunning at different sizes\n"
+        print "\nBelow should be the message [" + self.displayString  + "] in the middle of the terminal window with a border.\nTry rerunning at different sizes\n\n"
         cli.print_in_middle(self.displayString, "|")
-        print "\n"
+        self.seperator()
 
-    def test_remaining_functions(self):
-        
+    def test_big_message(self):
+        print "\nBelow should be the message [" + self.displayString  + "] displayed as a 'big message' \n\n"
+        cli.print_big_message(self.displayString)
+        self.seperator()
+
+    def test_warning_and_continue(self):
+        print "\n"
         cli.warning_and_continue("Test Warning Message")
-        cli.clear_screen()      
+        self.seperator()
+
+    def test_clear_screen(self):
+        print "Testing screen clear...\n\n"
+        cli.clear_screen()     
+        print "Screen should have cleared\n\n"
+        self.seperator()
+        
+    # z added so it runs last!
+    def test_z_quiting_and_aborting_methods(self):
+        self.autoAbortTest()
+        self.seperator()
+        self.error_and_quit_test()
+
+    def autoAbortTest(self):
+        print "Testing autoAbort method"
+        try:
+            cli.autoAbort()
+        except SystemExit, e:
+            self.assertEquals(e.code,0)
+            
+    def error_and_quit_test(self):
+        print "Testing error and quit"
+        try:
+            cli.error_and_quit()
+        except SystemExit, e:
+            self.assertEquals(e.code,1)
+
     
+        
